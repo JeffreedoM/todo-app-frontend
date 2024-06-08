9be52fc4f045ec93489d9090a8abb6158d091f99
Jeffrey Nunez <jeffreynunez029@gmail.com>
2024-06-08 11:54:24 +0800

    added padding

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ function App() {
   console.log(todos);
 
   return (
-    <div className="h-screen bg-gradient-to-r from-primary to-secondary font-pally-regular">
+    <div className="h-screen bg-gradient-to-r from-primary to-secondary px-2 font-pally-regular lg:px-0">
       <Navbar />
       <TodoForm />
       <div className="mx-auto mt-10 max-w-screen-md rounded-md bg-white px-4 py-2 font-pally-medium text-lg shadow-md">
         {todos.length > 0 ? (
           todos.map((todo) => <TodoList key={todo.id} todo={todo} />)
         ) : (
-          <div className="text-center my-4">
+          <div className="my-4 text-center">
             Your to-do list is empty. Add some tasks to get started!
           </div>
         )}
